37284c65cd0ab08d24289e4e4ff162683ee41c58
Mark <markolofsen@gmail.com>
2022-03-05 02:21:23 +0800

    fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,8 @@ module.exports = {
   title: 'UnrealOS',
   tagline: `With Pixel Streaming, you run a packaged Unreal Engine application on a desktop PC or a server in the cloud, along with a small stack of web services that are included with the Unreal Engine.`,
   url: 'https://markolofsen.github.io',
-  baseUrl: '/unrealos_doc/',
+  baseUrl: '/',
+  // baseUrl: '/unrealos_doc/', // for gh-pages
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
